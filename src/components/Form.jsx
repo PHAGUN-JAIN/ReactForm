552313f1e06cc179ea20004d81ed194ea1b3c501
PHAGUN-JAIN <omgill084@gmail.com>
2021-05-28 16:10:04 +0530

    form comonent updats

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,11 +3,18 @@ const input_style = {
     width: "30rem",
     lineHeight: "1.6rem"
 }
+
+const div_style = {
+    border: "2px solid black",
+    width:"500px",
+    margin: "0 auto",
+    padding:"1.2rem"
+}
 export default class Form extends Component {
     render(){
         return(
             <>
-            <div>
+            <div style={div_style}>
             <label htmlFor="name">First name:</label> <br />
             <input id="name" type="text" style={input_style}/><br /><br />
             <label htmlFor="lastname">Last Name:</label> <br />
@@ -30,6 +37,10 @@ export default class Form extends Component {
             <input id="contact" type="Number" style={input_style} />
             <br />
             <br />
+
+            <button type="submit">
+                SUBMIT
+            </button>
             </div>
             </>
         );
