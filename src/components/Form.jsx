a909b7b86cd0319ac9505affdb88b1b81e770950
PHAGUN-JAIN <omgill084@gmail.com>
2021-05-28 15:46:00 +0530

    style updates

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,16 +5,26 @@ export default class Form extends Component {
         return(
             <>
             <div>
-            <label htmlFor="name">NAME:</label> <br />
-            <input id="name" type="text" />
+            <label htmlFor="name">First name:</label> <br />
+            <input id="name" type="text" /><br /><br />
+            <label htmlFor="lastname">Last Name:</label> <br />
+            <input id="lasname" type="text" />
             <br />
             <br />
             <label htmlFor="branch">BRANCH:</label> <br />
             <input id="branch" type="text" />
             <br />
             <br />
-            <label htmlFor="sap">SAP ID:</label> <br />
+            <label htmlFor="sap">SAP Id:</label> <br />
             <input id="sap" type="text" />
+            <br />
+            <br />
+            <label htmlFor="email">Email Id:</label> <br />
+            <input id="email" type="email" />
+            <br />
+            <br />
+            <label htmlFor="contact">Contact:</label> <br />
+            <input id="contact" type="Number" />
             <br />
             <br />
             </div>
